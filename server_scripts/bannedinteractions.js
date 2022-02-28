8bc51688afaeda99d77af4567fe8b3dab78f8cfa
Polynomial <ricepoly262@gmail.com>
2022-02-27 22:37:21 -0800

    Update bannedinteractions.js
    Revert to older script until I can fix issues

--- server_scripts/bannedinteractions.js
+++ server_scripts/bannedinteractions.js
@@ -1,15 +1,15 @@
+//priority: -9999
+
 onEvent('block.place', e => {          
     let block = e.getBlock()
     let ply = e.getEntity()
     
     zjp_bannedinteractions.forEach(interaction => {
         let b1 = interaction[0]
-        let b2 = interaction[1]
 
-        if(block == b1 || block == b2){
-            if(block == b1){b2 = b2}
-            if(block == b2){b2 = b1}     
-               
+        if(block == b1){
+            let b2 = interaction[1]
+        
             let up = block.getUp()
             let down = block.getDown()
             let north = block.getNorth()
@@ -17,13 +17,14 @@ onEvent('block.place', e => {
             let east = block.getEast()
             let west = block.getWest()
             let a = [up,down,north,south,east,west]
+            
+            //ply.tell(`b2 event fired ${b2}`)
 
             a.forEach(testBlock => {
                 if(testBlock == b2){
                     ply.tell(Text.of(`Woah there ${ply}, you cant place that`).red())
                     ply.tell(Text.of(`${b1} on \n${b2}!`).darkRed())
-                    ply.tell(Text.of(`Attempts to circumvent will be met with a ban `).red())    
-                    console.log(`[ZJP] ${ply} attempted to place ${b} on ${b2}`)                         
+                    ply.tell(Text.of(`Attempts to circumvent will be met with a ban `).red())                           
                     e.cancel()
                 }
             })
@@ -34,8 +35,7 @@ onEvent('block.place', e => {
         if(block == b){
             ply.tell(Text.of(`Woah there ${ply}, you cant place that`).red())    
             ply.tell(Text.of(`${b}!`).darkRed())      
-            ply.tell(Text.of(`Attempts to circumvent will be met with a ban `).red()) 
-            console.log(`[ZJP] ${ply} attempted to place ${b}`)                
+            ply.tell(Text.of(`Attempts to circumvent will be met with a ban `).red())                 
             e.cancel()           
         }
 
